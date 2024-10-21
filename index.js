c87166b4e24f1ba7a2dd0be742f245b148a976a4
roman19730310 <polozunroman@gmail.com>
2024-10-21 13:39:27 +0300

    j.s. trening

---
+++ index.js
@@ -0,0 +1,44 @@
+//дужки - це оператор визову функції (
+ console.log(55);
+
+//інтерфейс-місце взаємодії між користувачем і системою
+let zero=null;
+let numb=5;
+let numb2=2;
+console.log(numb+numb2);
+let numb3=numb2-numb;
+console.log(numb3);
+console.log(zero+numb)
+let text='roman';
+console.log(text)
+let text2='text';
+console.log(text+text2);
+console.log(1/0);
+console.log(numb/text);
+const kub=10;
+const kub1=15;
+const kub2=kub*kub1;
+console.log(kub2);
+let число=3;
+let число1=6;
+let число2=8;
+if(6%2===0) {число2=число2+5}
+else{число=15};
+console.log(число);
+console.log(число1);
+console.log(число2);
+const namb =11;
+const namb1 =7;
+if(namb>=10) {
+    console.log('больше 10')
+}
+else{
+   console.log ("меньше 10")
+}
+if(namb>=10 && namb<=20){
+    console.log("в діапозоні")
+
+}
+else if(namb<10){
+    console.log("else if")
+}
